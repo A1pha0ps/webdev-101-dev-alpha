9ea71c277d981af1729a81e61b302cd4350d3af8
A1pha <majorzhen@gmail.com>
2023-03-04 21:50:54 -0500

    create post

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
-kevin
-
 <script>
 	import { currentUser } from '$lib/pocketbase';
+
+	let caption = '';
 </script>
+
+kevin
 
 {#if $currentUser}
 	<div class="flex justify-center">
@@ -13,7 +15,17 @@ kevin
 		<input type="checkbox" id="my-modal-4" class="modal-toggle" />
 		<label for="my-modal-4" class="modal cursor-pointer">
 			<label class="modal-box relative" for="">
-				<h3 class="text-lg font-bold">Create Post!</h3>
+				<div class="space-y-4">
+					<h3 class="text-lg font-bold">Create Post!</h3>
+					<div class="form-control">
+						<label class="label">
+							<span class="label-text">Caption</span>
+						</label>
+						<textarea class="textarea-bordered textarea h-24" bind:value={caption} />
+						<input type="file" class="file-input-bordered file-input w-full max-w-xs" />
+						<button class="btn-primary btn-block btn">Create post</button>
+					</div>
+				</div>
 			</label>
 		</label>
 	</div>
